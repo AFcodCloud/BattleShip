5c14b04b0735cba5a3528de4af93182c4d939ba9
AFcodCloud <alfibrz@gmail.com>
2020-05-10 09:42:32 +0800

    Functions  createFleet, collisionCheck, InvPos( environment check)

--- src/Components/Field.jsx
+++ src/Components/Field.jsx
@@ -3,16 +3,13 @@ import s from "./Field.module.css"
 import cn from "classnames"
 
 
-
-
-
-
+/* className={cn({[s.ship]:a.some((e)=> {return Number(e)===Number((i+""+j))})})}  */
 
 let tableCellElements=[]
 for(let i=0; i<10; i++){
     let cell=[];
     for(let j=0;j<10;j++){
-        cell.push(<td key={i+j} /* className={cn({[s.ship]:a.some((e)=> {return Number(e)===Number((i+""+j))})})} */ id={i+""+j}></td>)
+        cell.push(<td key={i+j}  className={cn({[s.ship]:fleet.flat().some(e=>{return e===(Number(i+""+j))})})}  id={Number(i+""+j)}></td>)
     }
     tableCellElements.push(<tr key={i} className={s.d} id={i}>
     {cell}
@@ -49,3 +46,59 @@ export default Field
 4 проверяю на пересечение
 5 добавляю корабль
 */
+/* 
+let fleet = []
+let ships=[[6, 16, 26, 36],
+[13, 14, 15],
+[65, 75, 85],
+[11, 12],
+[40, 41],
+[62, 72],
+[49],
+[87],
+[60],
+[29]]
+
+const createFleet=()=>{
+
+        for(let j=0;j<ships.length;j++){
+            let ship=ships; 
+                do{
+                    ship[j]
+                }while(CollisionCheck(ship[j]));
+                fleet.push(ship[j])
+        }       
+}
+
+
+const CollisionCheck=(ship)=>{
+    debugger
+        for(let i=0;i<ship.length; i++){
+            if(fleet.flat().indexOf(ship[i])<0){
+                return invPos(ship[i])
+            }
+    } false
+}
+
+const invPos=(pos)=>{
+    let splitPos=String(pos).split("")
+    let operands = [1,9,10,11];
+    let invPos=[]
+    if(Number(splitPos[1])===0||pos===0){
+        invPos.push(pos+1,pos-9,pos+10,pos-10,pos+11)
+    }else if(Number(splitPos[1])===9||pos===9){
+        invPos.push(pos-1,pos+9,pos+10,pos-10,pos-11)
+    }else{
+        for(let i=0; i<operands.length;i++){
+        invPos.push(pos+operands[i], pos-operands[i])
+    } 
+}
+    for(let j=0;j<invPos.length;j++){
+            if(fleet.flat().indexOf(invPos[j])>=0){
+                return true
+        }
+    }return false
+
+}
+
+createFleet()  */
--- src/Components/logicGame.js
+++ src/Components/logicGame.js
@@ -1,8 +1,8 @@
-const fieldSize=9;
-const ShipsCount=10;
-let ships=[]
+const fieldSize=10;
+const shipModels=[4,3,2,1]
+let fleet=[]
 
-let createShip=(lengthShip)=>{
+const createShip=(lengthShip)=>{
     let direction=Math.floor(Math.random()*2)//1-вертикальный 0-горизонтальный
     let row,column;//переменные для стартовой позиции корабля
     let ship=[];
@@ -15,27 +15,57 @@ let createShip=(lengthShip)=>{
     }
     for(let i=0; i<lengthShip; i++){
         if(direction===1){
-            ship.push(row+""+(column+i))
+            ship.push(Number(row+""+(column+i)))
         }else{
-            ship.push((row+i)+""+column)
+            ship.push(Number((row+i)+""+column))
         }
     }return ship;
 }
 
+const createFleet=()=>{ //функция для наполнения поля кораблями
+    for (let i=0;i<shipModels.length;i++){//первый цикл для прохода по моделям кораблей
+        for(let j=0;j<i+1;j++){//второй цикл для создания колличества кораблей конкретной модели
+            let ship; 
+                do{
+                    ship=createShip(shipModels[i])//функция для создания корабля принимет в аргументе колличество палуб
+                }while(CollisionCheck(ship));//функция для проверки на столкновения между кораблями на поле возвращает true или false 
+                fleet.push(ship) //если collisionCheck возвращает false корабль добавляется в массив
+        }
+    }
+}
 
-/* 
-1 вертикальный корабль должен начинаться по форммуле (высота таблицы - длина корабля)
-2 горизонтальный корабль должен начинаться по формулк (длина таблицы - длина корабля)
-3 1(4х палубный) 2(3х палубных) 3(2х палубных) 4 (1о палубных)
-4 нужен массив кораблей
-5 нужен массив значений расположения для каждого корабля 
-*/
 
-/* 
-1 генерирую стартовую позицию
-2 определяю направление
-3 пушу в массив 
-4 проверяю на пересечение
-5 добавляю корабль
-*/
+const CollisionCheck=(ship)=>{//проверка на столкновения принмает массив значений сгенерированных позиций корабля
+    let result;
+        for(let i=0;i<ship.length; i++){//цикл для прохода по каждой позиции 
+            if(fleet.flat().indexOf(ship[i])<0){//если значение не найдено то
+               result= invPos(ship[i])//присвоить переменной result результат проверки на соседние позиции для избежания прилипания кораблей друг к другу
+               if(result){return true}//если result = true прервать цикл вернув true для создания нового корабля
+            }else{return true}//если значение найдено то прерват цикл для создания нового корабля 
+    } 
+    return false; //если совпадения не найдены то вернуть false добавить корабль
+}
+
+const invPos=(pos)=>{//функция для проверки окружающих позиция (нужна чтобы избегать склеивание кораблей на поле)
+    let splitPos=String(pos).split("")
+    let operands = [1,9,10,11];
+    let invPos=[]
+    if(Number(splitPos[1])===0||pos===0){//если позиция находтся у левого борта поля нужно убрать некоторые проверки из окружающих клеток
+        invPos.push(pos+1,pos-9,pos+10,pos-10,pos+11)
+    }else if(Number(splitPos[1])===9||pos===9){//также убрать некоторые проверки для позиций у правого борта поля
+        invPos.push(pos-1,pos+9,pos+10,pos-10,pos-11)
+    }else{
+        for(let i=0; i<operands.length;i++){//если позции расположены не у левого и правого бортов нужно проверить все 8 позиций окружения
+        invPos.push(pos+operands[i], pos-operands[i])
+    } 
+}
+    for(let j=0;j<invPos.length;j++){//цикл для прохода по окружению каждой позици
+            if(fleet.flat().indexOf(invPos[j])>=0){//поиск в массиве кораблей совпадений по окружению
+                return true//прервать если в окружении имеется совпадение вернуться к созданию нового корабля
+        }
+    }
+return false;
+}
+
+
 
